5c5880848064377ee0d0d91538b2e0927def3fb0
Stephen Sinn <stevexian56@gmail.com>
2024-02-13 21:55:05 +0800

    dts: camera: Import Xiaomi changes

--- qcom/camera/config/waipio.mk
+++ qcom/camera/config/waipio.mk
@@ -2,7 +2,9 @@ dtbo-$(CONFIG_ARCH_WAIPIO) := waipio-camera.dtbo
 dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-overlay-v2.dtbo \
 								waipio-camera-sensor-mtp.dtbo \
 								waipio-camera-sensor-cdp.dtbo \
-								waipio-camera-sensor-qrd.dtbo
+								waipio-camera-sensor-qrd.dtbo \
+								zeus-sm8450-camera-sensor.dtbo \
+								cupid-sm8450-camera-sensor.dtbo
 
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera.dtbo
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera-sensor-idp.dtbo
@@ -11,7 +13,12 @@ dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera-sensor-qrd.dtbo
 dtbo-$(CONFIG_ARCH_CAPE) += cape-camera.dtbo
 dtbo-$(CONFIG_ARCH_CAPE) += cape-camera-sensor-mtp.dtbo \
 				cape-camera-sensor-cdp.dtbo \
-				cape-camera-sensor-qrd.dtbo
+				cape-camera-sensor-qrd.dtbo \
+				thor-sm8475-camera-sensor.dtbo \
+				diting-sm8475-camera-sensor.dtbo \
+				mayfly-sm8475-camera-sensor.dtbo \
+				unicorn-sm8475-camera-sensor.dtbo \
+				mondrian-sm8475-camera-sensor.dtbo
 
 dtbo-$(CONFIG_ARCH_CAPE) += ukee-camera-sensor-mtp.dtbo \
 				ukee-camera-sensor-cdp.dtbo \
